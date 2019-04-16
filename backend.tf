f05448c05e0616a7bbc02ff25372fca170f63b3a
Ryan Kilkenny <RK_72169@banfield.com>
2019-04-16 16:50:03 -0700

    added backend and provider

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+  storage_account_name = "terraformstatehco3vwseg1"
+  container_name       = "tfstate"
+  key                  = "a2e0904a-543c-4106-987d-e4ce5906c5cf.terraform.tfstate"
+  access_key           = "a+IULPwZnxYt2qgJC/hgKNfBNWSjvnhZAkxc5/UlTNRVF0RnJLE08R8TclgJB6M9rQ+h0bCHjEkU7Yp8fab1Gg=="
+  }
+}
